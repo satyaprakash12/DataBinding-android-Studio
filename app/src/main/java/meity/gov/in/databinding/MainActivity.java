e3f2b1caf168363378b70a3366603f805e3c8491
Satya Prakash <satyaprakas5555@gmail.com>
2022-06-15 00:01:10 -0700

    First Commit

---
+++ app/src/main/java/meity/gov/in/databinding/MainActivity.java
@@ -0,0 +1,21 @@
+package meity.gov.in.databinding;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import meity.gov.in.databinding.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityMainBinding activityMainBinding= DataBindingUtil.setContentView(this,R.layout.activity_main);
+    //activityMainBinding.setName("satya prakash");
+        Model model=new Model("Hi, My Name is Satya Prakash");
+        activityMainBinding.setModel(model);
+    }
+
+}
---
+++ app/src/main/java/meity/gov/in/databinding/Model.java
@@ -0,0 +1,16 @@
+package meity.gov.in.databinding;
+
+public class Model {
+    String name;
+    public Model(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
